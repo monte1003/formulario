e3e427394ef8b0de4b60b2f50908edd23cc225cb
unknown <isaacmonterrosaguzman@gmail.com>
2023-07-11 14:58:12 -0500

    proyecto finalizado

---
+++ script.js
@@ -0,0 +1,20 @@
+let signIn= document.getElementById('signIn')
+let signUp= document.getElementById('signUn')
+let nameInput= document.getElementById('nameInput')
+let title=document.getElementById('title')
+
+signIn.onclick=function(){
+    nameInput.style.maxHeight="0";
+    title.innerHTML="Login";
+    signUp.classList.add('disable')
+    signIn.classList.remove('disable')
+
+}
+
+signUp.onclick=function(){
+    nameInput.style.maxHeight="60px";
+    title.innerHTML="Registro";
+    signUp.classList.remove('disable')
+    signIn.classList.add('disable')
+
+}
